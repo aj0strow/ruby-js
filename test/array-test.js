b3787b682bfc153d21d8a3f2f73b85a2db79ba23
aj0strow <alexander.ostrow@gmail.com>
2013-02-03 06:23:34 -0500

    added Array ::new and at

--- test/array-test.js
+++ test/array-test.js
@@ -2,9 +2,38 @@
 
 var buster = require('buster');
 var ruby = require('../ruby');
+var _ = require('underscore');
 
-buster.testCase('Array testing', {
-    '::new': function () {
-      assert(Array.new() === []);
-    }
+buster.testCase('Array::new', {
+  'no args': function () {
+    assert( _.isEqual([], []) );
+  },
+  'size': function() {
+    assert( _.isEqual(Array.new(3), [null, null, null]) );
+  },
+  'default': function() {
+    assert( _.isEqual(Array.new(3, 'hi'), ['hi', 'hi', 'hi']) );
+  },
+  'block': function() {
+    var block = function(i) { return i * 2; };
+    assert( _.isEqual(Array.new(5, block), [0, 2, 4, 6, 8]) );
+  }
+});
+
+buster.testCase('Array#at', {
+  setUp: function() {
+    this.a = [1, 2, 3];
+  },
+  'out of range': function() {
+    assert( _.isEqual(null, this.a.at(6)) );
+    assert( _.isEqual(null, this.a.at(-4)) );
+  },
+  'positive index': function() {
+    assert( _.isEqual(1, this.a.at(0)) );
+    assert( _.isEqual(2, this.a.at(1)) );
+  },
+  'negative index': function() {
+    assert( _.isEqual(3, this.a.at(-1)) );
+    assert( _.isEqual(2, this.a.at(-2)) );
+  }
 });
--- test/object-test.js
+++ test/object-test.js
@@ -4,15 +4,15 @@ var buster = require('buster');
 var ary = require('../ruby');
 
 buster.testCase('Object', {
+  /*
     'is_a': function() {
-      assert({}.is_a('Object'));
-      assert([].is_a('Array'));
-      assert((function() {}).is_a('Function'));
+      assert( {}.is_a('Object') );
+      assert( (function() {}).is_a('Function') );
     },
+  */
     'class': function() {
-      assert.equal('Object', {}.class());
-      assert.equal('Array', [].class());
-      assert.equal('Number', Object(5).class())
+      assert( 'Object' === {}.class() );
+      assert( 'Number' === Object(5).class() );
     }
 });
 
